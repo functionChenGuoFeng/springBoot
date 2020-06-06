e99ae8cbccccd6afac845055beaee3b2a1e8f2fe
chenguofeng <1508194634@qq.com>
2020-06-06 10:53:03 +0800

    第二次提交springboot，增加输出

--- SpringBoot_02Quick/src/main/java/chen/guofeng/controller/QuickController.java
+++ SpringBoot_02Quick/src/main/java/chen/guofeng/controller/QuickController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuickController {
     @RequestMapping("/quick")
     public String quick(){
+        System.out.println("hello");
         return "hello";
     }
 }
